634269817ac3277ea46fc713903ec10ab2eb861b
gcisneros310 <gustavo.cisneros@student.csulb.edu>
2021-09-02 21:50:34 -0700

    basic build guide added

--- lib/pages/build_guide/build_guide_intro_page.dart
+++ lib/pages/build_guide/build_guide_intro_page.dart
@@ -21,21 +21,37 @@ class _BuildGuideIntro extends State<BuildGuideIntro> {
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
         backgroundColor: logoColor,
+        centerTitle: true,
         title: Text("PC Build Guide"),
         elevation: 0.0,
       ),
-      body: Center (
-          child: Text(
-              "Welcome to the PC Build Guide!\n\nNow that you have gathered all of"
-                  "your PC components, it is time to build it together!\n\n"
-                  "Building a PC might be daunting, but rest assured it is easier"
-                  "than expected, and this guide will help you through each individual step.",
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.white
-            ),
+      body: Container (
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+          margin: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Welcome to the PC Build Guide!\n\nNow that you have gathered all of "
+                    "your PC components, it is time to build it together!\n\n"
+                    "Building a PC might be daunting, but rest assured it is easier "
+                    "than expected, and this guide will help you through each individual step.\n\n"
+                    "This guide will include both text and visual descriptors for how to build your computer."
+                    ,
+                style: TextStyle(
+                    fontSize: 14.5,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    color: Colors.white
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                  child: Image(
+                      image: AssetImage('assets/buildguideintropc.png')
+                  )
+              )
+
+            ],
           )
       ),
 
--- lib/pages/build_guide/build_guide_list.dart
+++ lib/pages/build_guide/build_guide_list.dart
@@ -8,26 +8,76 @@ class BuildGuideList extends StatefulWidget {
 
 
 class _BuildGuideList extends State<BuildGuideList> {
-  var stepsListTitles = ["Step 1","Step 2","Step 3","Step 4","Step 5"];
+  var stepsListTitles = ["Step 0: Assemble your Parts",
+    "Step 1: Install your Power Supply",
+    "Step 2: Install the CPU",
+    "Step 3: Install the RAM",
+    "Step 4: Install the Motherboard",
+    "Step 5: Install the CPU Cooler/Heatsink",
+    "Step 6: Install the Graphics Card",
+    "Step 7: (Optional) Install any Optional Devices",
+    "Step 8: Install Storage Devices",
+    "Step 9: Review All Connections",
+    "Step 10: Power on your Computer!"
+
+  ];
   @override
   Widget build(BuildContext context) {
     final Color logoColor = Color(0xff66c290);
 
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("PC Build Guide"),
         backgroundColor: logoColor,
         elevation: 0.0,
       ),
-        body: ListView.builder(
-          itemCount: stepsListTitles.length,
-            itemBuilder: (context, index){
-              return ListTile(
-                onTap: (){},
-                title: Text(stepsListTitles[index])
-              );
-            }
-            )
+
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+            children: <Widget> [
+              Container(
+                padding: EdgeInsets.all(8),
+                margin: EdgeInsets.all(8),
+                child: Text(
+                    "Below are each step in order for how to build your PC one step at a time!\n\n"
+                        "Feel free to go back and forth in case you need to review how to follow a specific step.",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                    )
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                      itemCount: stepsListTitles.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              topRight: Radius.circular(10)
+                            ),
+                            side: BorderSide(width: 5, color: logoColor)
+                          ),
+                          child:ListTile(
+                            tileColor: Colors.greenAccent,
+                            onTap : (){},
+                            title: (Text(stepsListTitles[index])),
+                          )
+
+                        );
+                      }
+                  )
+              )
+
+            ]
+        )
+      )
+
     );
   }
 
