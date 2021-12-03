3c7e1046298e5ca1b2a2196176b288be42c84b66
Osman Khan <osmankhan902@gmail.com>
2021-12-02 19:40:31 -0800

    Updated build.gradle for firebase and added google services json

--- lib/pages/part_review/write_review.dart
+++ lib/pages/part_review/write_review.dart
@@ -50,149 +50,154 @@ class _ReviewWidgetState extends State<ReviewWidget> {
         elevation: 4,
       ),
       backgroundColor: Color(0xFFF5F5F5),
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-                child: Text(
-                  'Overall Rating',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800,
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          SafeArea(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                    child: Text(
+                      'Overall Rating',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-                  child: Row (
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                      child: Row (
 
-                      children: <Widget>[
-                        star = new StarRating(
-                          rating: rating,
-                          onRatingChanged: (rating) => setState(() => this.rating = rating),
-                        )
-                        ]
-                  )
-              ),
-            ),
-            SizedBox(height: 5),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-                child: Text(
-                  'Written Review',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800,
+                          children: <Widget>[
+                            star = new StarRating(
+                              rating: rating,
+                              onRatingChanged: (rating) => setState(() => this.rating = rating),
+                            )
+                            ]
+                      )
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-              child: TextFormField(
-                  controller: textController1,
-                  obscureText: false,
-                  maxLines: 2,
-                  decoration: InputDecoration(
-                    hintText: 'Add a title for your review',
-                    filled: true,
-                    border: OutlineInputBorder(),
-                    fillColor: logoColor,
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0x00000000),
-                        width: 1,
+                SizedBox(height: 5),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                    child: Text(
+                      'Written Review',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                        fontWeight: FontWeight.w800,
                       ),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(4.0),
-                        topRight: Radius.circular(4.0),
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0x00000000),
-                        width: 1,
-                      ),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(4.0),
-                        topRight: Radius.circular(4.0),
-                      ),
-                    ),
-                  ),
-                  style: GoogleFonts.exo2(color: Colors.black, fontSize: 18)
-              ),
-            ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
-              child: TextFormField(
-                controller: textController2,
-                obscureText: false,
-                  maxLines: 8,
-                decoration: InputDecoration(
-                  hintText: 'What did you like or dislike about the product?',
-                  filled: true,
-                  border: OutlineInputBorder(),
-                  fillColor: logoColor,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0x00000000),
-                      width: 1,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(4.0),
-                      topRight: Radius.circular(4.0),
-                    ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0x00000000),
-                      width: 1,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(4.0),
-                      topRight: Radius.circular(4.0),
                     ),
                   ),
                 ),
-                style: GoogleFonts.exo2(color: Colors.black, fontSize: 18)
-              ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                  child: TextFormField(
+                      controller: textController1,
+                      obscureText: false,
+                      maxLines: 2,
+                      decoration: InputDecoration(
+                        hintText: 'Add a title for your review',
+                        filled: true,
+                        border: OutlineInputBorder(),
+                        fillColor: logoColor,
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                      ),
+                      style: GoogleFonts.exo2(color: Colors.black, fontSize: 18)
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                  child: TextFormField(
+                    controller: textController2,
+                    obscureText: false,
+                      maxLines: 8,
+                    decoration: InputDecoration(
+                      hintText: 'What did you like or dislike about the product?',
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      fillColor: logoColor,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0x00000000),
+                          width: 1,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                    ),
+                    style: GoogleFonts.exo2(color: Colors.black, fontSize: 18)
+                  ),
+                ),
+                SizedBox(height: 20),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: 50,
+                  onPressed: () async{
+                    sample_review.review = textController2.text.toString();
+                    print(textController2.text.toString());
+                    sample_review.reviewTitle = textController1.text.toString();
+                    print(textController1.text.toString());
+                    sample_review.partRating = rating;
+                    sample_review.partName = widget.partName;
+                    print(widget.partName.hashCode);
+                    Map<String, dynamic> testmap = sample_review.toJson();
+                    await ref.child("reviews/").child(widget.partName.hashCode.toString()).push().update(testmap);
+                    Navigator.of(context).pop();
+                  },
+                  color: logoColor,
+                  child: Text('Submit',
+                      style: GoogleFonts.exo2(color: Colors.black, fontSize: 16)),
+                  textColor: Colors.black,
+                )
+              ],
             ),
-            SizedBox(height: 20),
-            MaterialButton(
-              elevation: 0,
-              minWidth: double.maxFinite,
-              height: 50,
-              onPressed: () async{
-                sample_review.review = textController2.text.toString();
-                print(textController2.text.toString());
-                sample_review.reviewTitle = textController1.text.toString();
-                print(textController1.text.toString());
-                sample_review.partRating = rating;
-                sample_review.partName = widget.partName;
-                print(widget.partName.hashCode);
-                Map<String, dynamic> testmap = sample_review.toJson();
-                await ref.child("reviews/").child(widget.partName.hashCode.toString()).push().update(testmap);
-                Navigator.of(context).pop();
-              },
-              color: logoColor,
-              child: Text('Submit',
-                  style: GoogleFonts.exo2(color: Colors.black, fontSize: 16)),
-              textColor: Colors.black,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
